a644a53cf8a4d3d9f9d2eb098f50d4933b58b551
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-10 21:30:12 -0500

    toggle functionality

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function contact(event) {
         })
     }
 
+let isModalOpen = false;
+
 function toggleModal() {
-    // toggle modal
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+    
 }
