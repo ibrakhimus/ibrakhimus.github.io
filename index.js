520f2056ae0c174e0225f9acb6c16f9a80e7e89d
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-10 20:19:47 -0500

    loading state modal overlay

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 // service_6hw7566
 // ETDn6akjv6lHFgQj0 
 
-
-
-
-
+function contact(event) {
+    event.preventDefault();
+    // emailjs
+    //     .sendForm(
+    //         "service_6hw7566",
+    //         "template_puut7uj",
+    //         event.target,
+    //         "ETDn6akjv6lHFgQj0"
+    //     ).then(() => {
+    //         alert("Thank you for your message!");
+    //     })
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible");
+        console.log("Thank you for your message!");
+    }, 1000);
+    }
